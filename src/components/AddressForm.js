e4212533e4bfa0d1ab7aefb852171ef51313aeae
kimjin <inclined37@gmail.com>
2023-08-15 22:57:05 +0900

    refactor: 주소 api 컴포넌트 분리 및 오류 수정

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -1,62 +1,68 @@
-import React, { useState } from "react";
-import DaumPostcode from "react-daum-postcode";
+import React, { useState, useCallback } from "react";
+import Post from "./Post";
+import { Col, Row } from "react-bootstrap";
 
-const AddressForm = () => {
+const AddressForm = ({ label, isZonecode }) => {
+  const [modalVisible, setModalVisible] = useState(false);
   const [zonecode, setZonecode] = useState("");
   const [address, setAddress] = useState("");
-  const [showModal, setShowModal] = useState(false);
 
-  const onChangeZoncode = (e) => {
-    setZonecode(e.target.value);
-  };
+  const openModal = useCallback(() => {
+    setModalVisible(true);
+  }, []);
 
-  const onChangeAddress = (e) => {
-    setAddress(e.target.value);
-  };
+  const closeModal = useCallback(() => {
+    setModalVisible(false);
+  }, []);
 
-  const onCompletePost = (data) => {
-    setZonecode(data.zonecode);
-    setAddress(data.address);
-    setShowModal(false);
-  };
-
-  const handleButtonClick = () => {
-    setShowModal(true);
+  // 선택된 주소를 주소 필드에 업데이트
+  const handleAddressSelected = ({ address, zonecode }) => {
+    setAddress(address);
+    setZonecode(zonecode);
+    closeModal();
   };
 
   return (
     <>
-      {/* 아래의 코드를 모달에 띄움 */}
-      <DaumPostcode
-        onComplete={onCompletePost}
-        autoClose={true}
-        animation={true}
-      />
-
-      <div>
-        {/* onChange Event로 set 으로 데이터 넣어주기 -> 모달창에 전달 */}
-        <input
-          type="text"
-          name="zonecode"
-          value={zonecode}
-          onChange={onChangeZoncode}
-          size={5}
-          readOnly
-        />
-        <input
-          type="text"
-          name="address"
-          value={address}
-          onChange={onChangeAddress}
-          size={45}
-          readOnly
-        />
-
-        {/* 버튼 클릭 시 모달창 보여줌 */}
-        <button type="button" onClick={handleButtonClick}>
-          주소검색
-        </button>
-      </div>
+      {/* Post 컴포넌트를 모달에 띄움, 우편번호, 상세주소, 주소 등을 가져옴. */}
+      {modalVisible && <Post onAddressSelected={handleAddressSelected} />}
+      <Row>
+        <Col>
+          <div>{label}</div>
+        </Col>
+        {/* 우편번호 */}
+        {isZonecode && (
+          <Col>
+            <div>
+              <input
+                type="text"
+                name="zonecode"
+                value={zonecode}
+                size={5}
+                readOnly
+              />
+            </div>
+          </Col>
+        )}
+        {/* 주소 */}
+        <Col>
+          <div>
+            <input
+              type="text"
+              name="address"
+              value={address}
+              size={45}
+              readOnly
+            />
+          </div>
+        </Col>
+        <Col>
+          {/* 버튼 클릭 시 Post 모달 호출 */}
+          <button type="button" onClick={openModal}>
+            주소검색
+          </button>
+        </Col>
+      </Row>
     </>
   );
 };
