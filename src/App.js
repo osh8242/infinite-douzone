1f01f1ef0a1e6675dcae9bb1fa0055c33b16ccd5
hyunsohyun <llikepsh515@gmail.com>
2023-08-16 10:53:39 +0900

    feat. 모달창 수정 완료, textbox 작업중

--- src/App.js
+++ src/App.js
@@ -1,49 +1,73 @@
 import { useState } from "react";
 import "./App.css";
-import ModalForm from "./components/ModalForm";
-
+import ModalComponent from "./components/ModalComponent";
 import { Button } from "react-bootstrap";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
-import ConfirmForm from "./components/ConfirmForm";
+import ConfirmComponent from "./components/ConfirmComponent";
+import TestFormComponent from "./components/TestFormComponent";
+import { saveConfirm } from "./util";
 
 function App() {
-  //model창 변수
-  const [modalShow, setModalShow] = useState(false);
+  const [modalState, setModalState] = useState({
+    show: false,
+    props: {},
+  });
 
-  //confirm창 변수
-  const [confimShow, setConfirmShow] = useState(false);
+  const [confirmState, setConfirmState] = useState({
+    show: false,
+    props: {},
+  });
 
+  /* confirm창 영역=========================================== */
+  const ConfirmClickHandler =()=>{
+    //confirm컴포넌트Props 추가 
+    const confirmAddProps = {
+      message: '삭제하시겠습니까?',      //메세지
+      onConfirm: onConfirm,            //확인 클릭 함수
+    };
+
+    setConfirmState({
+      show: true,
+      props: confirmAddProps,
+    });
+  }
+
+  //confirm창 확인버튼 함수 예시 코드
+  const onConfirm = () => {
+    alert('confirm창 확인을 누르셨습니다.');
+    setConfirmState({ ...confirmState, show: false });
+  } 
+
+  /* modal창 영역============================================== */
+  const ModalClickHandler =()=>{
+       const modalAddProps = {
+      title: '모달창제목',
+      size: 'lg',
+      backdrop: 'static',
+      animation: 'false',
+      onConfirm: () => {
+        alert('모달창 확인을 누르셨습니다.');
+        setModalState({ ...modalState, show: false });
+      },
+    };
+
+    setModalState({
+      show: true,
+      props: modalAddProps,
+    });
+  }
+  
   return (
     <>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
-        모달창 나와라 뿅
-      </Button>
+      <Button variant="primary" onClick={ModalClickHandler}>모달창 버튼</Button>
+      <Button variant="primary" onClick={ConfirmClickHandler}>confirm창 버튼</Button>
       
+      <ModalComponent show={modalState.show} onHide={() => setModalState({ ...modalState, show: false })} {...modalState.props}>
+        <TestFormComponent />
+      </ModalComponent>
 
-      <Button variant="primary" onClick={() => setConfirmShow(true)}>
-        yes or no
-      </Button>
-      
-      <ModalForm show={modalShow} onHide={() => setModalShow(false)} animation="false"/> 
-      <ConfirmForm show={confimShow} onHide={() => setConfirmShow(false)} /> 
-
-      {/* 
-        size = 안보내면 일반크기 500px
-              ,sm 작은창 300px
-              lg 큰창 	800px
-              ,xl 왕짱큰창 1140px
-
-        backdrop={"static"} 옵션 넣으면 모달창 이외 화면 클릭 안된다.
-        title={"모달창 제목"} 제목 넣기
-        animation={false} 애니메이션 없애기
-        
-        centered 위치 가운데
-        closeButton header에 close버튼옵션
-        form에 autoFocus 넣으면 focusing 효과가 난다
-
-        */}
+      <ConfirmComponent show={confirmState.show} onHide={() => setConfirmState({ ...confirmState, show: false })} {...confirmState.props} />
     </>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,7 @@
+export const saveConfirm = (onHide,onsave) => {
+    return {
+      message: '저장하시겠습니까?',
+      onsave:() => onsave(),
+      oncancel: () => onHide(),
+    };
+  };
