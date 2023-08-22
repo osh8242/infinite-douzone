06e7be6fa7f2583c2883e2de8d9f1a28979531bd
kimjin <inclined37@gmail.com>
2023-08-22 21:49:13 +0900

    refactor: 사원등록 페이지 main, sub 테이블 자료 해당하는 cdEmp로 가져오도록 수정

--- src/templates/EmpRegisterationLayout.js
+++ src/templates/EmpRegisterationLayout.js
@@ -1,6 +1,6 @@
 //작성자 김진
 import { Col, Row } from 'react-bootstrap';
-import React from 'react';
+import React, { useState } from 'react';
 import TableForm from '../components/TableForm';
 import MenuTab from '../components/MenuTab';
 import TextBoxComponent from '../components/TextBoxComponent';
@@ -13,6 +13,8 @@ import EmpRegisterationModel from '../model/EmpRegisterationModel';
 function EmpRegisterationLayout() {
   //Model로 관리되는 state들
   const {
+    cdEmp,
+    setCdEmp,
     leftTableData,
     setLeftTableData,
     mainTableData,
@@ -20,6 +22,8 @@ function EmpRegisterationLayout() {
     subTableData,
     setSubTableData,
   } = EmpRegisterationModel();
+
+  const [selectedMenu, setSelectedMenu] = useState(0);
 
   const menuList = ['기초자료', '가족사항'];
 
@@ -39,7 +43,11 @@ function EmpRegisterationLayout() {
         <Col md="3" id="empRegisterLayoutLeft">
           {/* 좌측 사원목록 테이블 */}
           {leftTableData ? ( //tableData가 준비되었을 경우에만 TableForm 컴포넌트 렌더링
-            <TableForm showCheckbox={true} tableData={leftTableData} />
+            <TableForm
+              showCheckbox={true}
+              tableData={leftTableData}
+              rowClickHandler={setCdEmp}
+            />
           ) : (
             <div>Loading...</div> //로딩중 화면 표시 내용
           )}
