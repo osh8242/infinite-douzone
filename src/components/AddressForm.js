c57947585c8f0f087f043249ff22a20abc564aec
kimjin <inclined37@gmail.com>
2023-08-22 15:54:31 +0900

    design: AddressForm 디자인 수정

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
-import ModalComponent from "./ModalComponent";
-import Post from "./Post";
+import React, { useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import ModalComponent from './ModalComponent';
+import Post from './Post';
+import '../styles/addressForm.css';
 
 const AddressForm = ({ label, isZonecode }) => {
-  const [zonecode, setZonecode] = useState("");
-  const [address, setAddress] = useState("");
+  const [zonecode, setZonecode] = useState('');
+  const [address, setAddress] = useState('');
 
   // 선택된 주소를 주소 필드에 업데이트
   const handleAddressSelected = ({ address, zonecode }) => {
@@ -21,61 +22,55 @@ const AddressForm = ({ label, isZonecode }) => {
 
   return (
     <>
-      <Row className="pt-3">
+      <Row className="py-1">
         <Col
-          md="2"
-          className="d-flex align-items-center justify-content-center"
-        >
-          <div>우편번호</div>
-        </Col>
-        {/* 우편번호 */}
-        {isZonecode && (
-          <Col
-            md="2"
-            className="d-flex align-items-center justify-content-center"
-          >
-            <div>
-              <Form.Control
-                type="text"
-                name="zonecode"
-                value={zonecode}
-                size={5}
-                disabled
-              />
-            </div>
-          </Col>
-        )}
-        <Col md="2">
-          {/* 버튼 클릭 시 Post 모달 호출 */}
-          <Button
-            variant="secondary"
-            onClick={() => setModalState({ ...modalState, show: true })}
-          >
-            주소검색
-          </Button>
-        </Col>
-      </Row>
-      <Row className="py-2">
-        <Col
-          md="2"
+          md="4"
           className="d-flex align-items-center justify-content-center"
         >
           <div>주소</div>
         </Col>
-        {/* 주소 */}
-        <Col md="4">
-          <div>
-            <Form.Control type="text" name="address" value={address} disabled />
-          </div>
-        </Col>
-        <Col md="6">
-          <div>
+        <Col md="8" id="fullAddressArea">
+          {/* 우편번호 */}
+          {isZonecode && (
             <Form.Control
+              id="zoneCodeArea"
               type="text"
-              name="address-detail"
-              placeholder="상세주소 입력"
+              name="zonecode"
+              value={zonecode}
+              // size={5}
+              disabled
             />
-          </div>
+          )}
+
+          {/* 주소 */}
+          <Form.Control
+            id="addressArea"
+            type="text"
+            name="address"
+            value={address}
+            disabled
+          />
+
+          {/* 버튼 클릭 시 Post 모달 호출 */}
+          <Button
+            id="addressSearchBtn"
+            variant="secondary"
+            onClick={() => setModalState({ ...modalState, show: true })}
+          >
+            검색
+          </Button>
+        </Col>
+      </Row>
+      {/* 상세주소 */}
+      <Row className="py-1">
+        <Col
+          md="4"
+          className="d-flex align-items-center justify-content-center"
+        >
+          <div>상세주소</div>
+        </Col>
+        <Col md="8">
+          <Form.Control type="text" name="address-detail" />
         </Col>
       </Row>
       <ModalComponent
