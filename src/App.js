50ae8f2e3e0ba9a7faff51ece0abf28131b61255
hyunsohyun <llikepsh515@gmail.com>
2023-08-22 11:40:31 +0900

    feat . pull전 임시백업

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./templates/Header";
-import LRlevel2Grid from "./templates/LRlevel2Grid";
+import LRlevel2Grid from "./templates/SalaryInformationEntry";
 
 function App() {
   return (
