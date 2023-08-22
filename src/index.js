e89d5ceae23647079b5e5a5613a91526f7d8a9f7
kimjin <inclined37@gmail.com>
2023-08-22 11:43:29 +0900

    design: header style 작업중

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,6 +2,22 @@
 import React, { Component } from "react";
 import { Button, Nav, Navbar } from "react-bootstrap";
 import Menubar from "../components/Menubar.js";
+import SelectForm from "../components/SelectForm.js";
+import TextBoxComponent from "../components/TextBoxComponent.js";
+import { border, color, display } from "@mui/system";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpRightFromSquare,
+  faBell,
+  faCalculator,
+  faPlus,
+  faPrint,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBellSlash,
+  faQuestionCircle,
+  faTrashCan,
+} from "@fortawesome/free-regular-svg-icons";
 
 class Header extends Component {
   constructor(props) {
@@ -16,9 +32,98 @@ class Header extends Component {
   };
 
   render() {
+    const optionList = [{ key: "더존비즈온", value: "더존비즈온" }];
+    const optionList2 = [
+      { key: "김회계 주임연구원", value: "김회계 주임연구원" },
+    ];
     return (
-      <>
-        <Navbar bg="light" expand="lg">
+      <div>
+        <div
+          style={{
+            background: "rgba(12,12,12,0.2)",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <SelectForm optionList={optionList} />
+            <SelectForm optionList={optionList} />
+          </div>
+          <div style={{ display: "flex", background: "rgba(12,12,12,0.2)" }}>
+            <TextBoxComponent placeholder={"찾고싶은 메뉴를 검색하세요"} />
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faPlus}
+                style={{ color: "rgb(57,63,80)" }}
+              />
+            </Button>
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faBell}
+                style={{ color: "rgb(57,63,80)" }}
+              />
+            </Button>
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faQuestionCircle}
+                style={{ color: "rgb(57,63,80)" }}
+              />
+            </Button>
+            <SelectForm optionList={optionList2} />
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            background: "rgb(48,150,255)",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <Button
+              onClick={this.toggleSidebar}
+              variant="outline-secondary"
+              style={{ background: "rgba(0,0,127,0.3)", border: "none" }}
+            >
+              <i className={`fa fa-bars`} style={{ color: "white" }} />
+            </Button>
+            {/* 로고 */}
+            <TextBoxComponent placeholder={"인사관리등록"} />
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                style={{ color: "#ffffff" }}
+              />
+            </Button>
+          </div>
+          <div style={{ display: "flex" }}>
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faPrint}
+                style={{ color: "rgb(57,63,80)", color: "white" }}
+              />
+            </Button>
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                style={{ color: "rgb(57,63,80)", color: "white" }}
+              />
+            </Button>
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faCalculator}
+                style={{ color: "rgb(57,63,80)", color: "white" }}
+              />
+            </Button>
+            <Button style={{ background: "none", border: "none" }}>
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                style={{ color: "rgb(57,63,80)", color: "white" }}
+              />
+            </Button>
+          </div>
+        </div>
+        {/* <Navbar bg="light" expand="lg">
           <Button
             className="mx-2"
             onClick={this.toggleSidebar}
@@ -36,8 +141,8 @@ class Header extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Menubar showSidebar={this.state.showSidebar} />
-      </>
+        <Menubar showSidebar={this.state.showSidebar} /> */}
+      </div>
     );
   }
 }
