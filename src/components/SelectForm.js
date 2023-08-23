90a01dd66778b93219b26d065f883539d9982334
kimjin <inclined37@gmail.com>
2023-08-23 21:25:14 +0900

    docs: SelectForm 컴포넌트 레이블 미기재시 빈공간 문제 수정

--- src/components/SelectForm.js
+++ src/components/SelectForm.js
@@ -12,16 +12,21 @@
   ]/> 
 */
 
-import { Col, Row } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
+import { Col, Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 function SelectForm({ label, optionList }) {
   return (
     <Row className="py-1">
-      <Col md="4" className="d-flex align-items-center justify-content-center">
-        <div>{label}</div>
-      </Col>
-      <Col md="8" className="d-flex align-items-center justify-content-center">
+      {label && (
+        <Col
+          md="4"
+          className="d-flex align-items-center justify-content-center"
+        >
+          <div>{label}</div>
+        </Col>
+      )}
+      <Col className="d-flex align-items-center justify-content-center">
         <Form.Select>
           {optionList.map((option, index) => (
             <option value={option.key} key={index}>
