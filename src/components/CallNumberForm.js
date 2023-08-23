23f97793191afed7249f4aa1ff48be53e3cebe25
kimjin <inclined37@gmail.com>
2023-08-23 21:23:31 +0900

    feat: 전화번호 형식 컴포넌트 추가

--- src/components/CallNumberForm.js
+++ src/components/CallNumberForm.js
@@ -1,42 +1,52 @@
 // 작성자: 김진
 // 용도: 전화번호 형식을 위한 TextBoxComponent
-
-// parameter: type, label, size, disabled, readOnly, plaintext, value
+/*
+  parameter: value1(cell1), value2(cell2), value3(cell3)
+  props:
+  type, label, size, disabled, readOnly, plaintext, value1, value2, value3
+*/
 
 import { Col, Form, Row } from 'react-bootstrap';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function CallNumberForm(props) {
   //props 속성들
-  const { type, label, size, disabled, readOnly, plaintext, value } = props;
+  const {
+    type,
+    label,
+    size,
+    disabled,
+    readOnly,
+    plaintext,
+    value1,
+    value2,
+    value3,
+  } = props;
 
-  //입력값
-  const [inputValue, setInputValue] = useState(value);
-
-  useEffect(() => {
-    setInputValue(props.value);
-  }, [value]);
-
-  //마스킹 함수
-  const handleInputValueChange = (event) => {
-    const input = event.target.value;
-
-    const maskedNumber = input.replace(
-      /^(\d{6})(\d+)/,
-      (match, group1, group2) => {
-        const maskedGroup2 = group2.replace(/./g, '*');
-        console.log('group1내용 : ' + group1);
-        return `${group1}-${maskedGroup2}`;
-      },
-    );
-
-    setInputValue(maskedNumber);
+  //////////////////////////////////////////////////////////
+  // 값을 수정하는 로직은 추후 수정예정
+  const handleValue1 = (e) => {
+    value1 = e.target.value;
   };
+  const handleValue2 = (e) => {
+    value2 = e.target.value;
+  };
+  const handleValue3 = (e) => {
+    value3 = e.target.value;
+  };
+  //////////////////////////////////////////////////////////
 
   return (
     <>
       <Row className="py-1">
-        {label && <Col md="4">{label}</Col>}
+        {label && (
+          <Col
+            md="4"
+            className="d-flex align-items-center justify-content-center"
+          >
+            {label}
+          </Col>
+        )}
         <Col className="d-flex align-items-center justify-content-center">
           <Form.Control
             type={type}
@@ -45,15 +55,33 @@ function CallNumberForm(props) {
             disabled={disabled}
             readOnly={readOnly}
             plaintext={plaintext}
-            value={inputValue}
-            onChange={handleInputValueChange}
+            value={value1}
+            onChange={handleValue1}
           ></Form.Control>
         </Col>
-        <Col>
-          <Form.Control></Form.Control>
+        <Col className="d-flex align-items-center justify-content-center">
+          <Form.Control
+            type={type}
+            placeholder={props.placeholder}
+            size={size}
+            disabled={disabled}
+            readOnly={readOnly}
+            plaintext={plaintext}
+            value={value2}
+            onChange={handleValue2}
+          ></Form.Control>
         </Col>
-        <Col>
-          <Form.Control></Form.Control>
+        <Col className="d-flex align-items-center justify-content-center">
+          <Form.Control
+            type={type}
+            placeholder={props.placeholder}
+            size={size}
+            disabled={disabled}
+            readOnly={readOnly}
+            plaintext={plaintext}
+            value={value3}
+            onChange={handleValue3}
+          ></Form.Control>
         </Col>
       </Row>
     </>
