9f8bc0a532564c70c4aece6d2459346976988e5f
kimjin <inclined37@gmail.com>
2023-08-23 21:29:22 +0900

    design: EmpRegister 컴포넌트 레이아웃 수정 및 컴포넌트 추가

--- src/templates/EmpRegisterationLayout.js
+++ src/templates/EmpRegisterationLayout.js
@@ -1,14 +1,20 @@
-//작성자 김진
+// 작성자: 김진
+// 사원등록 페이지 전용 레이아웃
+
 import { Col, Row } from 'react-bootstrap';
 import React, { useState } from 'react';
 import TableForm from '../components/TableForm';
 import MenuTab from '../components/MenuTab';
 import TextBoxComponent from '../components/TextBoxComponent';
 import DateForm from '../components/DateForm';
-import SelectForm from '../components/SelectForm';
 import AddressForm from '../components/AddressForm';
 import '../styles/empRegisterationLayout.css';
 import EmpRegisterationModel from '../model/EmpRegisterationModel';
+import CommonConstant from '../model/CommonConstant';
+import CallNumberForm from '../components/CallNumberForm';
+import NoSocialFormForEmpRegister from '../components/NoSocialFormForEmpRegister';
+import EmailForm from '../components/EmailForm';
+import DateTest from '../components/DateTest';
 
 function EmpRegisterationLayout() {
   //Model로 관리되는 state들
@@ -23,19 +29,17 @@ function EmpRegisterationLayout() {
     setSubTableData,
   } = EmpRegisterationModel();
 
-  const [selectedMenu, setSelectedMenu] = useState(0);
+  //고정된 값을 가지는 state들
+  const {
+    labels,
+    mainTabMenuListForEmpRegister,
+    ynForList,
+    genderRadioList,
+    emailList,
+  } = CommonConstant();
 
-  const menuList = ['기초자료', '가족사항'];
-
-  const genderList = [
-    { key: '0', value: '0.남성' },
-    { key: '1', value: '1.여성' },
-  ];
-
-  const residentList = [
-    { key: '0', value: '0.거주' },
-    { key: '1', value: '1.미거주' },
-  ];
+  // 메뉴 탭 전환 기능 추후 수정 예정
+  // const [selectedMenu, setSelectedMenu] = useState(0);
 
   return (
     <>
@@ -54,94 +58,120 @@ function EmpRegisterationLayout() {
         </Col>
         <Col md="6" id="empRegisterLayoutRight">
           <Row id="empDataSortedMenuArea">
-            <MenuTab menuList={menuList} />
+            <MenuTab menuList={mainTabMenuListForEmpRegister} />
             <div id="empDataSortedLine"></div>
           </Row>
           {/* 사원정보 편집 */}
           <Row id="baseData">
             {mainTableData ? (
               <div>
-                <DateForm label={'입사일자'} value={mainTableData.daEnter} />
-                <TextBoxComponent
-                  label={'주민번호'}
+                <DateTest
+                  label={labels.daEnterForEmpRegister}
+                  defaultValue={mainTableData.daEnter}
+                />
+                <NoSocialFormForEmpRegister
+                  label={labels.noSocial}
+                  optionList1={ynForList}
+                  optionList2={genderRadioList}
                   value={mainTableData.noSocial}
                 />
                 <TextBoxComponent
-                  label={'거주지국'}
-                  value={mainTableData.addNation}
+                  label={labels.addNation}
+                  value={mainTableData.abbNation}
                 />
                 <TextBoxComponent
-                  label={'국적'}
+                  label={labels.cdNation}
                   value={mainTableData.cdNation}
                 />
                 <AddressForm
-                  label={'주소'}
                   isZonecode={true}
-                  value={(mainTableData.addHome, mainTableData.addHome2)}
-                />
-                <TextBoxComponent
-                  label={'전화번호'}
-                  value={
-                    mainTableData.telHome1 +
-                    '-' +
-                    mainTableData.telHome2 +
-                    '-' +
-                    mainTableData.telHome3
+                  zipHomeData={
+                    mainTableData.zipHome ? mainTableData.zipHome : null
+                  }
+                  addHome1Data={
+                    mainTableData.addHome1 ? mainTableData.addHome1 : null
+                  }
+                  addHome2Data={
+                    mainTableData.addHome2 ? mainTableData.addHome2 : null
                   }
                 />
-                <TextBoxComponent
-                  label={'모바일번호'}
-                  value={
-                    mainTableData.celEmp1 +
-                    '-' +
-                    mainTableData.celEmp2 +
-                    '-' +
-                    mainTableData.celEmp3
-                  }
+                <CallNumberForm
+                  label={labels.telHome}
+                  value1={mainTableData.telHome1}
+                  value2={mainTableData.telHome2}
+                  value3={mainTableData.telHome3}
                 />
-                <TextBoxComponent
-                  label={'이메일'}
+                <CallNumberForm
+                  label={labels.calEmp}
+                  value1={mainTableData.celEmp1}
+                  value2={mainTableData.celEmp2}
+                  value3={mainTableData.celEmp3}
+                />
+                <EmailForm
+                  label={labels.emEmp}
                   value={mainTableData.emEmp}
+                  optionList={emailList}
                 />
                 <TextBoxComponent
-                  label={'메신저ID'}
+                  label={labels.idMsn}
                   value={mainTableData.idMsn}
                 />
-                <TextBoxComponent label={'부서'} value={mainTableData.cdDept} />
                 <TextBoxComponent
-                  label={'직종'}
+                  label={labels.cdDept}
+                  value={mainTableData.cdDept}
+                />
+                <TextBoxComponent
+                  label={labels.cdOccup}
                   value={mainTableData.cdOccup}
                 />
-                <TextBoxComponent label={'직급'} value={mainTableData.rankNo} />
                 <TextBoxComponent
-                  label={'호봉'}
+                  label={labels.cdOffpos}
+                  value={mainTableData.rankNo}
+                />
+                <TextBoxComponent
+                  label={labels.cdSalcls}
                   value={mainTableData.cdSalcls}
                 />
                 <TextBoxComponent
-                  label={'현장'}
+                  label={labels.cdField}
                   value={mainTableData.cdField}
                 />
                 <TextBoxComponent
-                  label={'프로젝트'}
+                  label={labels.cdProject}
                   value={mainTableData.cdProject}
                 />
-                <DateForm label={'퇴사일자'} value={mainTableData.daRetire} />
-                <TextBoxComponent
-                  label={'급여이체은행'}
-                  value={mainTableData.cdBank}
+                {mainTableData.daRetire ? (
+                  <DateForm
+                    label={labels.daRetire}
+                    value={mainTableData.daRetire}
+                  />
+                ) : (
+                  <TextBoxComponent
+                    label={labels.daRetire}
+                    placeholder={'----년 --월 --일'}
+                    disabled={'disabled'}
+                  />
+                )}
+                <CallNumberForm
+                  label={labels.cdBank}
+                  value1={mainTableData.cdBank}
+                  value2={mainTableData.noBnkacct}
+                  value3={mainTableData.nmKrname}
                 />
               </div>
             ) : (
               <div>Loading...</div>
             )}
           </Row>
-          <Row id="familyData">
-            {subTableData ? (
-              <TableForm showCheckbox={true} tableData={subTableData} />
-            ) : (
-              <div>Loading...</div>
-            )}
-          </Row>
+          <div style={{ display: 'none' }}>
+            <Row id="familyData">
+              {subTableData ? (
+                <TableForm showCheckbox={true} tableData={subTableData} />
+              ) : (
+                <div>Loading...</div>
+              )}
+            </Row>
+          </div>
         </Col>
       </Row>
     </>
