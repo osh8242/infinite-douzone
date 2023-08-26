2523c4b1e3bc8ff3fc68e8338ed69c24d410b5b6
hyunsohyun <llikepsh515@gmail.com>
2023-08-26 16:42:02 +0900

    feat: textbox 컴포넌트 수정작업중 (주민번호, 유효성검사 등등)

--- src/components/TextBoxComponent.js
+++ src/components/TextBoxComponent.js
@@ -2,29 +2,29 @@ import { faC } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
-
+import { makeCommaNumber, makePlainNumber } from "../utils/NumberUtils";
 
 function TextBoxComponent(props) {
-  
   /* props 속성들*/
   const {
     type,     //textbox, regNum, email, password, file, date, color... 
+
     id,
     name,
     label, 
     size, 
     value,    
-
     suffix,   // %, 원화표시
     mask,     // '*'
-
-    rows,             //textarea 전용 (몇행짜리 textbox만들거니)
-    truncateLength,  //mask 전용 (처음 몇 글자만 보여줄 것인지 설정)
+    
+    rows,             //textarea 전용 (몇행짜리 textbox)
+    // truncateLength,  //mask 전용 (처음 몇 글자만 보여줄 것인지 설정)
 
     //이벤트
     onChange,
     onClick,
     onClickCodeHelper,
+    onFocus,
 
     //유효성 검사
     validationFunction,
@@ -39,110 +39,139 @@ function TextBoxComponent(props) {
 
   // 입력값
   const [inputValue, setInputValue] = useState(value);
-  
+
   const handleInputChange = (event) => {
+    event.preventDefault();
     const newValue = event.target.value;
-      //if (validation(newValue)) {
-        setInputValue(makeProcessedValue(newValue));
-      //}
+      if (validation(event.target, newValue)) {       //유효성
+        setInputValue(makeProcessedValue(newValue));  //show data
+      }
   }
 
-
-  // plain데이터 넘기기 +  보여줄 형태로 데이터 가공 
   const makeProcessedValue = (newValue) => {
     let processedValue = newValue;
-
-    // suffix 단위 붙이기
-    if(suffix){
-      const numValue = processedValue.replaceAll(suffix, '');
-      onChange && onChange(numValue); //no suffix value 넘기기
-      processedValue = numValue + suffix; // 받은 값에 suffix 추가
+  
+    if (suffix || thousandSeparator) {
+      suffix && ( processedValue = processSuffix(processedValue, suffix))
+      thousandSeparator && (processedValue = processThousandSeparator(processedValue))
+      onChange && onChange(makePlainNumber(processedValue));// onChange 이벤트 처리
     }
 
-    // thousandSeparator 세자리 콤마
-    if(thousandSeparator){ 
-      const numValue = processedValue.replaceAll(',', '');
-      onChange && onChange(numValue); //no comma value 넘기기
-      processedValue = numValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','); // 받은 값에 3자리수마다 콤마를 추가
+    if(type==='regNum'){
+      processedValue =  (/^\d{0,6}$/.test(newValue))? (newValue.replace(/(\d{6})(\d{0,1})/, '$1-$2')):(newValue) //하이픈 넣기
+      onChange && onChange(processedValue);
     }
-
-    // mask 마스킹 처리
-    if(mask){
-      onChange && onChange(newValue); //no masking value 넘기기
-      processedValue = maskString(newValue);
-    }
-
+  
     return processedValue;
-  }
+  };
+  
+  const processSuffix = (value, suffix) => {
+    const numValue = value.replaceAll(suffix, '');
+    return numValue + suffix;
+  };
+  
+  const processThousandSeparator = (value) => {
+    const numValue = value.replaceAll(/,/g, '');
+    return makeCommaNumber(numValue.toString());
+  };
 
-  //마스킹 String 만들기 함수 
-  const maskString = (input) => {
+  // 마스킹 String 만들기 함수 
+  const maskString = (truncateLength,input) => {
     const visibleCharacters = truncateLength; // 처음 몇 글자를 보여줄 것인지 설정
     const maskedPortion = input.slice(visibleCharacters).replace(/\S/g, mask);
     return input.slice(0, visibleCharacters) + maskedPortion;
   };
 
+  
   //유효성 검사
-  const validation = (value) => {
-     //props에서 받은 유효성검사 
-     validationFunction && validationFunction(value)
-
+  const validation = (object ,value) => {
+    
     if(thousandSeparator || suffix){
+      thousandSeparator && (value = value.replaceAll(/,/g, ''));
+      suffix && (value = value.replaceAll(suffix, ''));
+      
       const isNumber = /^\d+$/.test(value);
-      if (!isNumber){
-        alert("숫자를 입력해주세요."); 
-        //focus input
+      if (!isNumber){ 
+        alert("숫자만 입력해주세요.");  
+        //object.value='';
+        //object.focus(); 
+        return false;
       }
     }
 
-    if(type === 'regNum'){
-      //주민등록번호 유효성
-      
+    if(type === 'regNum'){//주민등록번호 유효성
+      if(value.length >= 14){ 
+        alert("13자리만 입력해주세요."); 
+        object.value = value.slice(0, 14);
+      }
     }
+
+    //validationFunction && validationFunction(value);
+    return true;
   }
 
-
-
+  const handleInputFocus = (e) => {
+    const obj = e.target;
+   
+    onFocus && onFocus(obj);// onFocus 이벤트 처리
+  }
+ 
   return (
     <Row className="py-1">
-      {label && (
+      {label ? (
+        <>
         <Col md="4" className="d-flex align-items-center justify-content-center" >
           <div>{label}</div>
         </Col>
+        <Col md="8" className="d-flex align-items-center justify-content-center">
+          {codeHelper ? (
+            <div className="react-datepicker__input-container">
+              {renderFormControl()} <FontAwesomeIcon icon={faC} onClick={onClickCodeHelper}/>
+            </div>
+          ) : (
+            <>
+              {renderFormControl()}
+            </>
+          )}
+        </Col>
+        </>
+      ):(
+        <>{renderFormControl()}</>
       )}
-      
-      <Col md="8" className="d-flex align-items-center justify-content-center">
-        {type === 'textarea' ? (
-          <Form.Control 
-            as="textarea" 
-            id={id} 
-            name={name} 
-            rows={rows} 
-            onChange={handleInputChange}
-            onClick={onClick}/>
-       ) : (
-          <>  
-          <Form.Control
-            type={type}
-            id={id} 
-            name={name}
-            size={size}
-            disabled={disabled}
-            readOnly={readOnly}
-            plaintext={plaintext}
-            value={inputValue}
-            // onChange = {type === 'regnum'? {handleRegNumChange} : {handleInputChange}}
-            onChange={handleInputChange}
-            onClick={onClick}
-          />
-          </>
-        )}
-       {codeHelper && <FontAwesomeIcon icon={faC} onClick={onClickCodeHelper}/>}
-      </Col>
     </Row>
     
   );
 
+  function renderFormControl() {
+    if (type === 'textarea') {
+      return (
+        <Form.Control 
+          as="textarea" 
+          id={id} 
+          name={name} 
+          rows={rows} 
+          onChange={handleInputChange}
+          onClick={onClick}
+        />
+      );
+    } else {
+      return (
+        <Form.Control
+          type={type}
+          id={id} 
+          name={name}
+          size={size}
+          disabled={disabled}
+          readOnly={readOnly}
+          plaintext={plaintext}
+          value={inputValue}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          onClick={onClick}
+        />
+      );
+    }
+  }
 }
 
 export default TextBoxComponent;
