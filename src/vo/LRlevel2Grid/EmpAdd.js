3e9f6fb4cb2b7e58c581b7d6f261d5971cb0bdad
hyunsohyun <llikepsh515@gmail.com>
2023-08-28 11:51:19 +0900

    feat: 충돌

---
+++ src/vo/LRlevel2Grid/EmpAdd.js
@@ -0,0 +1,9 @@
+const EmpAdd = (empAddData) => {
+    // item: {
+    //   [labels.cdEmp]: item.cdEmp,
+    //   [labels.nmKrname]: item.nmKrname,
+    // }
+    return { empAddData };
+  };
+  
+  export default EmpAdd;
