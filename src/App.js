b0efe98cfdb26576bc2e0e077c3a30728785529e
hyunsohyun <llikepsh515@gmail.com>
2023-08-30 07:38:52 +0900

    feat: pull

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./templates/Header";
-import LRlevel2Grid from "./templates/LRlevel2Grid";
+import SalaryInformationEntry from "./templates/SalaryInformationEntry";
 
 function App() {
   return (
     <div>
       <Header />
       <Container>
-        <LRlevel2Grid />
+        <SalaryInformationEntry />
       </Container>
     </div>
   );
