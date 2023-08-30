d1f61546acb0eaa935f588c309aed6d8e4906742
hyunsohyun <llikepsh515@gmail.com>
2023-08-30 14:36:20 +0900

    refactor: confirm 창 모달 수정

--- src/components/ConfirmComponent.js
+++ src/components/ConfirmComponent.js
@@ -1,15 +1,39 @@
+/*
+  현소현
+  const [confirmState, setConfirmState] = useState({
+    show: false,
+    props: {},
+  });
+
+  const ConfirmClickHandler =()=>{
+    setConfirmState({
+      show: true,
+      props: confirmAddProps,
+    });
+  }
+
+  const onConfirm = () => {
+    확인후 처리할 함수
+    setConfirmState({ ...confirmState, show: false });
+  } 
+
+  <Button variant="primary" onClick={ConfirmClickHandler}>confirm창 버튼</Button>
+  <ConfirmComponent show={confirmState.show} onHide={() => setConfirmState({ ...confirmState, show: false })} message="삭제하시겠습니까?" onConfirm={onConfirm} />
+
+*/
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function ConfirmComponent(props) {
+  const { show, onHide, message, onConfirm} = props;
   
   return (
-    <Modal {...props} backdrop="static" centered>
-      <Modal.Body className="grid-example"> {props.message} </Modal.Body>
+    <Modal show={show} backdrop="static" centered>
+      <Modal.Body className="grid-example"> {message} </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>취소</Button>
-        <Button variant="primary" onClick={props.onConfirm}>확인</Button>
+        <Button variant="secondary" onClick={onHide}>취소</Button>
+        <Button variant="primary" onClick={onConfirm}>확인</Button>
       </Modal.Footer>
     </Modal>
   );
