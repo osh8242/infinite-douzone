538ff2874930c6670a20fcb1774b30511fb789b0
hyunsohyun <llikepsh515@gmail.com>
2023-08-31 03:53:38 +0900

    feat: app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { Container } from "react-bootstrap";
 import "./App.scss";
-import { ContextProvider } from "./model/ContextModel";
 import Header from "./templates/Header";
 import SalaryInformationEntry from "./templates/SalaryInformationEntry";
 
@@ -8,9 +6,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Container>
         <SalaryInformationEntry />
-      </Container>
     </div>
   );
 }
