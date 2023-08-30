f23ffa12655e26dee4fc9524fde9a80123b37f64
hyunsohyun <llikepsh515@gmail.com>
2023-08-30 14:21:24 +0900

    refactor:  모달창

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,28 +1,38 @@
+/* 현소현
+const [modalState, setModalState] = useState({ show: false , modalData: null });  //모달창
+
+<ModalComponent title= {'코드도움'} show={modalState.show} onHide={() => setModalState({ ...modalState, show: false })} size="lg" centered>
+    <p>안녕하세요우!</p> //children
+</ModalComponent> 
+
+ */
+
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 function ModalComponent(props) {
+  const { children, title, onHide, onConfirm, size, backdrop, animation,} = props;
   return (
     <Modal
       {...props}
-      size={props.size}
-      backdrop={props.backdrop}
-      animation={props.animation}
+      size={size}
+      backdrop={backdrop}
+      animation={animation}
       centered
     >
       <Modal.Header>
-        <Modal.Title>{props.title}</Modal.Title>
+        <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
 
-      <Modal.Body>{props.children}</Modal.Body>
+      <Modal.Body>{children}</Modal.Body>
 
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>
+        <Button variant="secondary" onClick={onHide}>
           닫기
         </Button>
-        {props.onConfirm && (
-          <Button variant="primary" onClick={props.onConfirm}>
+        {onConfirm && (
+          <Button variant="primary" onClick={onConfirm}>
             확인
           </Button>
         )}
