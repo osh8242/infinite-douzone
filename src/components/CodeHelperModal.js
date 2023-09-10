cc784d0b6d03a433523ae501e9ec17eae0a3e922
hyunsohyun <llikepsh515@gmail.com>
2023-09-11 01:05:37 +0900

    feat:  인사관리 header 추가

--- src/components/CodeHelperModal.js
+++ src/components/CodeHelperModal.js
@@ -1,113 +1,46 @@
-/* 현소현
+/* 현소현  코드도움창 */
 
-  ex)
-  <CodeHelperModal
-    show = {state.modalState.show}   
-    onHide = {() => actions.setModalState({ ...state.modalState, show: false })} 
-    onConfirm = {() => alert('확인')}
-    apiFlag = {true} or {false}
-    setRowData = {actions.setAddRow}
-    usePk = {''}
-    tableData = {setTableData()}
-  />
-  
- */
-
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Form, Row, Table } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import { objectToQueryString } from "../utils/StringUtils";
+import { Form, Row } from "react-bootstrap";
 import TextBoxComponent from "./TextBoxComponent";
-import TableForm from "./TableForm";
 
+import PropTypes from 'prop-types';
+import TableForm from "./TableForm";
+import ModalComponent from "./ModalComponent";
+import CommonConstant from "../model/CommonConstant";
+
+const {labels} = CommonConstant();
 function CodeHelperModal(props) {
   const {
-    show, // [필수] 모달창 열기 modalstate -> true
-    onHide, // [필수] 모달창 닫기 modalstate -> false
-    onConfirm, // [선택]
-    onRowClick, // [선택]
+    show,
+    onHide, 
 
+    subject,
     setRowData, // [필수] 객체 반환 받을 set함수
-    usePk, // [선택] rowData에서 특정 필드값을 set할거면 usePk='칼럼명' 설정... row(객체 전체)를 set할거면 false
-
-    // [apiFlag, codeHelperCode]나 codeHelperCodes중 둘중 하나는 반드시 사용해야함
-
-    codeHelperCode, // [apiFlag = true일때 필수] 관리 편리성 위해 url, pacodeHelperCode를 미리 만들어두기
-
-    table, // [선택] api 안쏠때 프런트에 저장되어있는 tabledata를 이용해서 형식에 맞게 만들어둔
+    onConfirm,  // [선택] 확인버튼
+    
+    tableHeaders,
+    tableData,
+    usePk,      // [선택] rowData에서 특정 필드값을 set할거면 usePk='칼럼명' 설정... row(객체 전체)를 set할거면 false
+    searchField
   } = props;
-
-  //const { state, actions } =  ModalModel();
-
-  const [modalData, setTModalData] = useState({
-    title: "",
-    tableHeaders: [{ field: "codeId", text: "코드" }],
-    // { field: "code", text: "Code"},
-    // { field: "nmKrname", text: "사원명"},
-    // { field: "noSocial", text: "주민(외국인)번호"},
-    // { field: "daRetire", text: "퇴사일자"}],
-    tableData: [],
-    //searchField : ['pk'],
-    searchField: ["codeId", "codeName"],
-  });
 
   const [searchTerm, setSearchTerm] = useState("");
   const [oriData, setOriData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-  //const [table, setTable] = useState([]);
 
   useEffect(() => {
-    if(codeHelperCode){
-      const initialTableData =  getCodeListForCodeHelper(codeHelperCode)
-      setTModalData((prevData) => ({
-        ...prevData,
-        tableData: initialTableData,
-      }));
-    }else {
-      setTModalData(table);
-    }
-    setOriData(modalData.tableData);
-    setFilteredData(modalData.tableData);
-  },[]);
+    // console.log(tableData);
+    setOriData(tableData);
+    setFilteredData(tableData);
+  }, [tableData]);
 
-
-  const getCodeListForCodeHelper = async (codeHelperCode) => {
-    const url = "http://localhost:8888";
-  
-    if (codeHelperCode !== "") {
-      try {
-        const response = await axios.get(
-          url + codeHelperCode.url + objectToQueryString(codeHelperCode.params),
-          { "Content-Type": "application/json" }
-        );
-  
-        const codeDataList = response.data.map((object) => {
-          const dynamicProperties = { item: {} };
-          for (const key in object) {
-            dynamicProperties.item[key] = object[key];
-          }
-          return dynamicProperties;
-        });
-  
-        return codeDataList;
-
-      } catch (error) {
-        console.log("에러발생: ", error);
-        // 에러 처리
-      }
-    }
-  };
-
-  /* table.searchField에 해당하는 field만 검색 */
+  // table.searchField에 해당하는 field만 검색
   useEffect(() => {
     if (searchTerm !== "") {
-      console.log("modalData", modalData);
-      console.log("searchTerm", searchTerm);
       setFilteredData(
         oriData.filter((row) => {
-          return modalData.searchField.some((field) =>
+          return searchField.some((field) =>
             row.item[field].toLowerCase().includes(searchTerm.toLowerCase())
           );
         })
@@ -115,91 +48,63 @@ function CodeHelperModal(props) {
     } else {
       setFilteredData(oriData);
     }
+    
   }, [searchTerm]);
 
-  /* 클릭한 행반환 */
+  // 클릭한 행반환
   const handleRowClick = (row) => {
     setSearchTerm("");
-    if (setRowData) {
-     usePk ? setRowData(row.item[usePk]) : setRowData(row.item);
-    }
-    onRowClick && onRowClick();
+    if (setRowData) usePk ? setRowData(row.item[usePk]) : setRowData(row.item)
     onHide();
   };
 
   return (
     <>
-    {console.log({modalData})}
-      <Modal show={show} size="lg" centered>
-        <Modal.Header>
-          <Modal.Title>{modalData.title}</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <div className="container">
-            <Row>
-              {/* <Table striped bordered hover>
-              <thead>
-                <tr onClick={(e) => handleRowClick(e)}>
-                  {modalData.tableHeaders.map((header) => (
-                    <th key={header.field}>{header.text}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {filteredData.map((row, rowIndex) => (
-                  //<tr key={row.pk} className={selectedRow === row.pk ? 'selected' : ''} onClick={() => handleRowClick(row)}>
-                  <tr key={rowIndex} onClick={() => handleRowClick(row)}>
-                    {modalData.tableHeaders.map((header) => (
-                      <td key={header.field}>{row[header.field]}</td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </Table> */}
-              {/* <TableForm
+    <ModalComponent title= {subject} show={show} onHide={onHide} 
+      onConfirm={onConfirm} size="lg" centered>
+        <div>
+          <Row>
+            <TableForm
               readOnly
-              tableHeaders={modalData.tableHeaders}
+              tableHeaders={tableHeaders}
               tableData={filteredData}
               onRowClick={handleRowClick}
-            /> */}
-            </Row>
-            <Row>
-              <Form.Group>
-                <TextBoxComponent
-                  type="text"
-                  label={"찾을내용"}
-                  value={searchTerm}
-                  onChange={setSearchTerm}
-                />
-              </Form.Group>
-            </Row>
-          </div>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={(e) => {
-              onHide();
-            }}
-          >
-            닫기
-          </Button>
-          {onConfirm && (
-            <Button
-              variant="primary"
-              onClick={(e) => {
-                onConfirm();
-              }}
-            >
-              확인
-            </Button>
-          )}
-        </Modal.Footer>
-      </Modal>
-    </>
+            />
+          </Row>
+          <Row>
+            <Form.Group>
+              <TextBoxComponent
+                type="text"
+                label={labels.searchText}
+                value={searchTerm}
+                onChange={setSearchTerm}
+              />
+            </Form.Group>
+          </Row>
+        </div>
+    </ModalComponent>
+   </>
   );
 }
+
+CodeHelperModal.defaultProps = {
+  show : true,
+  onHide : null,
+  tableHeaders : [],
+  tableData: [ {item:{}} ],
+  subject: '',
+  searchField : [],
+};
+
+CodeHelperModal.propTypes = {
+  tableHeaders: PropTypes.array.isRequired,
+  tableData: PropTypes.arrayOf(
+    PropTypes.shape({
+      item: PropTypes.object.isRequired,
+    })
+  ).isRequired,
+  subject: PropTypes.string,
+  searchField: PropTypes.arrayOf(PropTypes.string),
+};
 
 export default CodeHelperModal;
