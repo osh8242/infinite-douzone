a1528286c29bdf0351ee4040e05abe84f6c25685
hyunsohyun <llikepsh515@gmail.com>
2023-09-30 20:30:25 +0900

    feat: 로딩바 구현중

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import LoadingProvider from "./Loading/LoadingProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-  <App />,
+  <LoadingProvider>
+  <App/>
+  </LoadingProvider>
   //</React.StrictMode>
 );
 
