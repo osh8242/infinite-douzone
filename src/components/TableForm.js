cb933070996684424e5b30eba756aac56403b1e1
osh <osh8242@gmail.com>
2023-10-04 22:48:57 +0900

    refactor: tableForm 코드헬퍼 코드값 찾기

--- src/components/TableForm.js
+++ src/components/TableForm.js
@@ -1,7 +1,17 @@
-import { faPlus, faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlus,
+  faSortDown,
+  faSortUp,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Table } from "react-bootstrap";
 import { CODE_VALUE } from "../model/CommonConstant";
 import "../styles/tableForm.css";
@@ -182,7 +192,9 @@ const TableForm = ({
 
   // 현재 테이블의 모든 인풋요소들을 가져옴
   const getInputElements = useCallback((event, rowIndex, columnIndex) => {
-    return tbodyRef.current.children[rowIndex].querySelectorAll("input");
+    return tbodyRef.current.children[rowIndex].querySelectorAll(
+      "input, select"
+    );
   }, []);
 
   // 새로운 행(빈행)을 만드는 함수
@@ -324,10 +336,7 @@ const TableForm = ({
       // 각 입력 필드의 값을 editedRow에 업데이트
       currentRowInputs.forEach((input, index) => {
         const field = input.id;
-        const targetIndex = tableHeaders.findIndex(
-          (thead, index) => thead.field === input.id
-        );
-        const type = tableHeaders[targetIndex]?.type;
+        const type = tableHeaders[index]?.type;
         if (type && type === "textCodeHelper") {
           console.log("findKeyInCodeValue", findKeyInCodeValue(input.value));
           editedRow.item[field] = findKeyInCodeValue(input.value);
@@ -537,7 +546,9 @@ const TableForm = ({
         case "textCodeHelper":
           let codeHelperData = codeHelper[field];
           let tableData = codeHelperData.tableData;
-          let targetIndex = tableData.findIndex((row) => row.item[field] === value);
+          let targetIndex = tableData.findIndex(
+            (row) => row.item[field] === value
+          );
           const newField = field.charAt(0).toUpperCase() + field.slice(1);
           return targetIndex !== -1
             ? tableData[targetIndex].item[`nm${newField}`]
@@ -585,7 +596,8 @@ const TableForm = ({
               if (event.shiftKey) {
                 if (columnRef > 0) setColumnRef(columnRef - 1);
               } else {
-                if (columnRef < tableHeaders.length - 1) setColumnRef(columnRef + 1);
+                if (columnRef < tableHeaders.length - 1)
+                  setColumnRef(columnRef + 1);
               }
               break;
             default:
@@ -618,7 +630,8 @@ const TableForm = ({
 
             case "ArrowRight":
               event.preventDefault();
-              if (columnRef < tableHeaders.length - 1) setColumnRef(columnRef + 1);
+              if (columnRef < tableHeaders.length - 1)
+                setColumnRef(columnRef + 1);
               break;
 
             case "Home":
@@ -725,7 +738,9 @@ const TableForm = ({
               <th
                 className="tableHeader"
                 data-field={thead.field}
-                onClick={sortable ? (e) => rowsOrderHandler(e, thead.field) : null}
+                onClick={
+                  sortable ? (e) => rowsOrderHandler(e, thead.field) : null
+                }
                 key={rowIndex}
                 style={thead.width && { width: thead.width }}
               >
@@ -810,7 +825,9 @@ const TableForm = ({
                 <td
                   className={getTdClassName(tableRows.length, columnIndex)}
                   key={columnIndex}
-                  onClick={(e) => handleRowClick(e, tableRows.length, columnIndex)}
+                  onClick={(e) =>
+                    handleRowClick(e, tableRows.length, columnIndex)
+                  }
                 >
                   <div className="tableContents">
                     {!showCheckbox && columnIndex === 0 && (
