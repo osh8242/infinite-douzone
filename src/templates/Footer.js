f2fa8910247c1036fa8604f18885deef5cf7ccd2
kimjin <inclined37@gmail.com>
2023-10-05 17:01:26 +0900

    design: button 글자크기 10pt로 변경 및 footer작업

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -1,23 +1,27 @@
 import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import "../styles/fonts.css";
 import "../styles/footer.css";
+import "../styles/fonts.css";
 
 export default function Footer() {
   return (
-    <div className="SUITE p-12 px-5 footer">
-      <hr />
-      <div>INFINITE DOUZONE</div>
-      <div className="team-member p-10">
-        <p>오승환</p>
-        <p>이서연</p>
-        <p>현소현</p>
-        <p>김진</p>
-        <p style={{ marginLeft: "10px" }}>
+    <div id="footer-area">
+      <div id="footer" className="SUITE p-12 px-5">
+        <div>INFINITE DOUZONE</div>
+        <div id="footer-copyright">
           Copyright <FontAwesomeIcon icon={faCopyright} /> INFINITE DOUZONE. All
           rights reserved.
-        </p>
+        </div>
+        {/* <div id="team-member" className="p-10">
+          <div>오승환</div>
+          <div>|</div>
+          <div>이서연</div>
+          <div>|</div>
+          <div>현소현</div>
+          <div>|</div>
+          <div>김진</div>
+        </div> */}
       </div>
     </div>
   );
