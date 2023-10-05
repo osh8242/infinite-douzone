17f9c959efbd5188b7ea22387321b2f1c86d5b56
kimjin <inclined37@gmail.com>
2023-10-05 09:06:09 +0900

    design: footer 꾸미는중...

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -2,23 +2,23 @@ import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "../styles/fonts.css";
+import "../styles/footer.css";
 
 export default function Footer() {
   return (
-    <div className="Jost p-10 px-5">
+    <div className="SUITE p-12 px-5 footer">
       <hr />
-      <br />
-      <p>INFINITE DOUZONE</p>
-      <div className="d-flex gap-2">
-        <p>Seunghwan Oh</p>
-        <p>Seoyeon Lee</p>
-        <p>Sohyun Hyun</p>
-        <p>Jin Kim</p>
+      <div>INFINITE DOUZONE</div>
+      <div className="team-member p-10">
+        <p>오승환</p>
+        <p>이서연</p>
+        <p>현소현</p>
+        <p>김진</p>
+        <p>
+          Copyright <FontAwesomeIcon icon={faCopyright} /> INFINITE DOUZONE. All
+          rights reserved.
+        </p>
       </div>
-      <p>
-        Copyright <FontAwesomeIcon icon={faCopyright} /> INFINITE DOUZONE. All
-        rights reserved.
-      </p>
     </div>
   );
 }
