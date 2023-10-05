39c7094ef49f85f67c105eeb8c4d773ac120b0c2
kimjin <inclined37@gmail.com>
2023-10-05 17:09:17 +0900

    design: footer 디자인 완료

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -3,26 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "../styles/footer.css";
 import "../styles/fonts.css";
+import { useLocation } from "react-router-dom";
 
 export default function Footer() {
+  const location = useLocation();
+  const currentPath = location.pathname; // 현재 페이지 경로
+  const excludedPages = [
+    "/",
+    "/login",
+    "/loginFindId",
+    "/loginFindPwd",
+    "/signup",
+    "/error",
+    "/successSignup",
+  ];
+  const isExcludedPage = excludedPages.includes(currentPath);
+
   return (
-    <div id="footer-area">
-      <div id="footer" className="SUITE p-12 px-5">
-        <div>INFINITE DOUZONE</div>
-        <div id="footer-copyright">
-          Copyright <FontAwesomeIcon icon={faCopyright} /> INFINITE DOUZONE. All
-          rights reserved.
+    <>
+      {!isExcludedPage && (
+        <div id="footer-area">
+          <div id="footer" className="SUITE p-12 px-5">
+            <div>INFINITE DOUZONE</div>
+            <div id="footer-copyright">
+              Copyright <FontAwesomeIcon icon={faCopyright} /> INFINITE DOUZONE.
+              All rights reserved.
+            </div>
+          </div>
         </div>
-        {/* <div id="team-member" className="p-10">
-          <div>오승환</div>
-          <div>|</div>
-          <div>이서연</div>
-          <div>|</div>
-          <div>현소현</div>
-          <div>|</div>
-          <div>김진</div>
-        </div> */}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
