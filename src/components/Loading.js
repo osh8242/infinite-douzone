2a1cc71662a31d22fedce10db58a7f4ba0ca1651
kimjin <inclined37@gmail.com>
2023-10-10 10:28:35 +0900

    design: 로딩바 배경제거..

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import spanner from "../styles/img/loading-spinner.gif";
+import React from "react";
+import spanner from "../styles/img/loading-spinner-unscreen.gif";
 
 const Loading = () => {
   const modalStyle = {
-    position: 'fixed',
-    top: '0',
-    left: '0',
-    width: '100vw',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     //backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    zIndex: '9999',
+    zIndex: "9999",
   };
 
   const contentStyle = {
-    position: 'absolute', // 절대 위치로 설정
-    bottom: '20px', // 아래에서 20px 떨어진 위치
-    right: '20px', // 오른쪽에서 20px 떨어진 위치
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-end', // 컨텐츠를 오른쪽으로 정렬
-    padding: '20px',
-    borderRadius: '10px',
-    boxShadow: '0px 0px 10px rgba(255, 255, 255, 0.2)',
+    position: "absolute", // 절대 위치로 설정
+    bottom: "20px", // 아래에서 20px 떨어진 위치
+    right: "20px", // 오른쪽에서 20px 떨어진 위치
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end", // 컨텐츠를 오른쪽으로 정렬
+    padding: "20px",
+    borderRadius: "10px",
+    boxShadow: "0px 0px 10px rgba(255, 255, 255, 0.2)",
   };
 
   return (
     <div style={modalStyle}>
       <div style={contentStyle}>
-        <h3>Loading</h3>
+        {/* <h3>Loading</h3> */}
         <img id="loading" src={spanner} alt="" />
       </div>
     </div>
